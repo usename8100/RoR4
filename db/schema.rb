f10e1f8ab3a202a4b51602b848f73ca6c35f303b
TrinhMinhLong <trinhminhlongad@gmail.com>
2021-08-12 23:11:40 +0700

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_082651) do
+ActiveRecord::Schema.define(version: 2021_08_12_153455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_08_10_082651) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
