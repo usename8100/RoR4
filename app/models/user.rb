39d25ca7cad42f27405c115e63f69a305b09e355
TrinhMinhLong <trinhminhlongad@gmail.com>
2021-08-16 21:30:50 +0700

    make it better

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 50 },
 	format: { with: VALID_EMAIL_REGEX },
-	uniqueness: { case_sensitive: false }
+	uniqueness: { case_sensitive: true }
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
 end
