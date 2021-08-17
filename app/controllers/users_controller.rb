5b49acd7ca8006de495ec25eb26ebf081806f9f1
TrinhMinhLong <trinhminhlongad@gmail.com>
2021-08-17 21:44:32 +0700

    login logout page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
 
     #respond_to do |format|
       if @user.save
+        log_in @user
         #format.html { redirect_to @user, notice: "User was successfully created." }
         #format.json { render :show, status: :created, location: @user }
         flash[:success] = "Sign up successfully!"
