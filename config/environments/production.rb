ef32b046e7151d0e3a9823e60085bed25803a803
TrinhMinhLong <trinhminhlongad@gmail.com>
2021-08-25 01:31:58 +0700

    fix production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,8 +7,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'tranquil-waters-82690.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { :host => 'tranquil-waters-82690.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
