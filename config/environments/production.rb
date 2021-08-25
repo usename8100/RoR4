e9c9c2ab8475bd9d21bd8f006e470182fd64c254
TrinhMinhLong <trinhminhlongad@gmail.com>
2021-08-25 17:01:39 +0700

    fix host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -130,8 +130,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => "trinhminhlongad@gmail.com",
-   :password             => "xsxtbtzivmmcyage",
+   :user_name            => ENV['SMTP_USER_NAME'],,
+   :password             => ENV['SMTP_PASSWORD'],
    :authentication       => "plain",
    :domain               => 'heroku.com',
    :enable_starttls_auto => true
