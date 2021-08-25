710ce1dc2c6102d6b399eb2351dbde9caaa32032
TrinhMinhLong <trinhminhlongad@gmail.com>
2021-08-25 15:41:15 +0700

    fix send active mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
     #respond_to do |format|
       if @user.save
         ExampleMailer.sample_email(@user).deliver
-        @user.send_activation_email
+        #@user.send_activation_email
         #msg = "http://localhost:3000/account_activations/" + @user.activation_token + "/edit?email=" + @user.email.gsub("@","%40")
         msg2 = "https://tranquil-waters-82690.herokuapp.com/account_activations/" + @user.activation_token + "/edit?email=" + @user.email.gsub("@","%40")
         flash[:info] = "Please check your email to activate your account.\n"
