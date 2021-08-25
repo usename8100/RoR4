b6b5406d9c5815033dcfeec10a75cf319ad53234
TrinhMinhLong <trinhminhlongad@gmail.com>
2021-08-25 15:26:34 +0700

    display active link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,10 +35,10 @@ class UsersController < ApplicationController
         ExampleMailer.sample_email(@user).deliver
         @user.send_activation_email
         #msg = "http://localhost:3000/account_activations/" + @user.activation_token + "/edit?email=" + @user.email.gsub("@","%40")
-        #msg2 = "https://tranquil-waters-82690.herokuapp.com/account_activations/" + @user.activation_token + "/edit?email=" + @user.email.gsub("@","%40")
+        msg2 = "https://tranquil-waters-82690.herokuapp.com/account_activations/" + @user.activation_token + "/edit?email=" + @user.email.gsub("@","%40")
         flash[:info] = "Please check your email to activate your account.\n"
         #flash[:success] = msg
-        #flash[:danger] = msg2
+        flash[:danger] = msg2
         redirect_to root_url
       else
         render 'new'
