9ff538f863cb5e280c48a8047efc976ecac65403
TrinhMinhLong <trinhminhlongad@gmail.com>
2021-08-25 23:23:44 +0700

    remove image var

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,7 @@ class User < ApplicationRecord
 	uniqueness: { case_sensitive: true }
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-	validates :image, 
-    content_type: { 
-      in: %w[image/jpeg image/gif image/png], message: "must be a valid image format"
-    },
-    size:{ 
-      less_than: 5.megabytes,
-      message: "should be less than 5MB" 
-    }
+	
 
 	# Returns a resized image for display.
 	def display_image
