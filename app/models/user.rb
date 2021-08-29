0b540e31122a0c1593d7530734b853c5d2b0fe53
TrinhMinhLong <trinhminhlongad@gmail.com>
2021-08-29 23:52:30 +0700

    optimize validation notices

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
 	format: { with: VALID_EMAIL_REGEX },
 	uniqueness: { case_sensitive: true }
 	has_secure_password
-	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+	validates :password, presence: true, length: { minimum: 6 }
 	
 	# Returns the hash digest of the given string.
 	def User.digest(string)
